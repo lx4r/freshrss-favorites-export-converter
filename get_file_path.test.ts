088e77ae48fdd3404900d84cbc98f3106f29e963
lx4r <lx4r@users.noreply.github.com>
2021-03-08 22:21:14 +0100

    refactor: rephrasing names of tests for get_file_path.ts

--- get_file_path.test.ts
+++ get_file_path.test.ts
@@ -4,13 +4,13 @@ import {
 } from "https://deno.land/std@0.89.0/testing/asserts.ts";
 import { getFilePath } from "./get_file_path.ts";
 
-Deno.test("getFilePath(): no arguments passed", () => {
+Deno.test("getFilePath(): throws when no arguments passed", () => {
   assertThrows(() => getFilePath([]), undefined, "no JSON file specified");
 });
-Deno.test("getFilePath(): empty file name passed", () => {
+Deno.test("getFilePath(): throws when empty file name passed", () => {
   assertThrows(() => getFilePath([""]), undefined, "empty JSON file specified");
 });
-Deno.test("getFilePath(): valid file name passed", () => {
+Deno.test("getFilePath(): gets file path when valid file name passed", () => {
   const jsonFileName = "my_file.json";
   assertStrictEquals(getFilePath([jsonFileName]), jsonFileName);
 });
