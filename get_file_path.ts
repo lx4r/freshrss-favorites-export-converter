33e2fbe274a8bf3de5aac33a537794f18c821f52
lx4r <lx4r@users.noreply.github.com>
2021-03-09 18:30:22 +0100

    feat: more specific error message when no file path is specified

--- get_file_path.ts
+++ get_file_path.ts
@@ -1,10 +1,10 @@
 export function getFilePath(scriptArgs: string[]): string {
   if (scriptArgs.length < 1) {
-    throw new Error("no JSON file specified");
+    throw new Error("No path to the JSON file specified.");
   }
   const filename = scriptArgs[0];
   if (filename === "") {
-    throw new Error("empty JSON file specified");
+    throw new Error("Empty path to the JSON file specified");
   }
   return filename;
 }
