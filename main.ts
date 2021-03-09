9253041325297630152c6cf1e44a295439b2ae0d
lx4r <lx4r@users.noreply.github.com>
2021-03-09 17:58:36 +0100

    feat: logging names of saved files

--- main.ts
+++ main.ts
@@ -3,8 +3,12 @@ import { getFilePath } from "./get_file_path.ts";
 import { generateHTMLForDisplay } from "./generate_html_for_display.ts";
 import { generateHTMLForImport } from "./generate_html_for_import.ts";
 
-const FILE_FOR_IMPORT_PATH = "freshrss_favorites_for_firefox_import.html";
-const FILE_FOR_DISPLAY_PATH = "freshrss_favorites_for_display.html";
+const FILE_FOR_IMPORT_NAME = "freshrss_favorites_for_firefox_import.html";
+const FILE_FOR_DISPLAY_NAME = "freshrss_favorites_for_display.html";
+
+function logFileExported(fileName: string, filePurpose: string) {
+  console.log(`${filePurpose} successfully saved as ${fileName}`);
+}
 
 const filePath = getFilePath(Deno.args);
 getFavoritesFileContent(Deno.readTextFile, filePath)
@@ -13,14 +17,16 @@ getFavoritesFileContent(Deno.readTextFile, filePath)
     const htmlForDisplay = generateHTMLForDisplay(
       filterFavoriteInfo(parsedContent)
     );
-    await Deno.writeTextFile(FILE_FOR_DISPLAY_PATH, htmlForDisplay);
+    await Deno.writeTextFile(FILE_FOR_DISPLAY_NAME, htmlForDisplay);
+    logFileExported(FILE_FOR_DISPLAY_NAME, "HTML for display");
     return parsedContent;
   })
   .then(async (parsedContent) => {
     const htmlForImport = generateHTMLForImport(
       filterFavoriteInfo(parsedContent)
     );
-    await Deno.writeTextFile(FILE_FOR_IMPORT_PATH, htmlForImport);
-    return parsedContent;
+    await Deno.writeTextFile(FILE_FOR_IMPORT_NAME, htmlForImport);
+    logFileExported(FILE_FOR_IMPORT_NAME, "HTML for import into Firefox");
   })
-  .then((_) => console.log("Files successfully written"));
+  .then((_) => console.log("Export finished successfully."))
+  .catch((reason) => console.error("An error occurred. Reason:", reason));
